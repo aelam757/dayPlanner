aa2fb5edb94bf8264733932ed948c0f05c3f32b6
Alan Elam <aelam757@gmail.com>
2020-01-12 22:01:46 -0500

    still trying to catch up

---
+++ script.js
@@ -0,0 +1,42 @@
+// start with document.ready function the whole file will live in this function listen for save button clicks
+
+//capture the value of the time input and save it to local storage
+
+//append each saved item from local storage to the page in correct input moment js
+
+//capture the time of each div via its id in the html and compare the mometjs(current time) to the time in each div and change the colour for each input depending off whether they were before during or coming up
+
+let textInput = 
+
+
+
+
+$(document).ready(function () {
+
+    $("#currentDay").text(moment().format("MMM, Do YYYY") );
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
